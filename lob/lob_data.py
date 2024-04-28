36581b8d3a1bd92e91930233e1eaf8b0ee08d149
Javier Sabio Gonzalez <javier@javiers-mbp.lan>
2024-04-28 12:55:43 +0200

    changed lob data to include all orders that produced the update

--- lob/lob_data.py
+++ lob/lob_data.py
@@ -11,15 +11,13 @@ class LOBupdate:
         bid_volumes (list[int]): A list of volumes corresponding to the bid prices.
         ask_prices (list[float]): A list of ask prices.
         ask_volumes (list[int]): A list of volumes corresponding to the ask prices.
-        trade_price (float, optional): The price at which the last trade occurred, if any.
-        trade_size (int, optional): The size of the last trade, if any.
+        orders (list[Order]): The list of orders that produced the update
     """
 
-    def __init__(self, time, bid_prices, bid_volumes, ask_prices, ask_volumes, trade_price=None, trade_size=None):
+    def __init__(self, time, bid_prices, bid_volumes, ask_prices, ask_volumes, orders):
         self.time = time
         self.bid_prices = bid_prices
         self.bid_volumes = bid_volumes
         self.ask_prices = ask_prices
         self.ask_volumes = ask_volumes
-        self.trade_price = trade_price
-        self.trade_size = trade_size
+        self.orders = orders
